98259051596f89d4f9f24cdc5067645ee1143668
akz92 <aridemoulin@gmail.com>
2015-07-19 18:40:07 -0400

    Revert "solve another 2 security issues"
    This reverts commit afe2e88b0c365e099b0e92903a6ebb7da7214415.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def show
    # filter the params[:id] here to allow only certain values like
-   #if params[:id].match /browse|help/
-   #  render :partial => params[:id]
+   if params[:id].match /browse|help/
+     render :partial => params[:id]
    #else
    #  render :file => "/path/to/some/404_template", :status => 404
-   #end
+   end
   end
 
   def help
