ab59afe5782fdd8d77d2bb37cb1522f20fdd4180
akz92 <aridemoulin@gmail.com>
2015-07-23 18:41:34 -0400

    modal forms and accordions to subjects/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,8 @@ module ApplicationHelper
     end
   end
 
+  #def resource_name :user end 
+  #def resource \@resource ||= User.new end 
+  #def devise_mapping @devise_mapping ||= Devise.mappings[:user] end 
+  #def resource_class User end 
 end
