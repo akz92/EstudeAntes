0f5f679f6c5941b7e36a0ebd3f7bd82669545e92
akz92 <aridemoulin@gmail.com>
2015-07-31 13:41:34 -0400

    added tests for relations

--- spec/models/events_spec.rb
+++ spec/models/events_spec.rb
@@ -9,6 +9,8 @@ describe Event do
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :every }
   it { is_expected.to validate_inclusion_of(:every).in_array(["month", "week", "day"]) }
+  it { is_expected.to belong_to :subject }
+  it { is_expected.to have_one(:period).through(:subject) }
 
   it "is invalid if start_date after end_date" do
     event= Event.new(start_date: "05-01-2015", end_date: "01-01-2015", start_time: "02:00:00", end_time: "03:00:00", title: "titulo", every: "week")
--- spec/models/tests_spec.rb
+++ spec/models/tests_spec.rb
@@ -5,6 +5,7 @@ describe Test do
   it { is_expected.to validate_numericality_of :value }
   it { is_expected.to validate_numericality_of :grade }
   it { is_expected.to validate_numericality_of :subject_id }
+  it { is_expected.to belong_to :subject }
 
   it "is invalid if grade is higher than value" do
     test = Test.new(date: "01-01-2015", grade: 10, value: 5)
