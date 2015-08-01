a5b55fbf1f7a8e3cde96852c5e3c54c91c634737
akz92 <aridemoulin@gmail.com>
2015-08-01 09:41:46 -0300

    fixed calendr not showing with less than 2 subjects

--- app/assets/javascripts/periods.js
+++ app/assets/javascripts/periods.js
@@ -1,5 +1,45 @@
-//$('document').ready(function(){
 function js(){
+  $('#calendar').fullCalendar({
+    defaultView: "agendaWeek", 
+    theme: true,
+    height: "auto",
+    themeButtonIcons: false,
+    header: {
+      left: 'today prev,next',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    monthNames: ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    allDaySlot: false,
+    slotDuration: "01:00:00",
+    axisFormat: 'HH:mm',
+    timeFormat: 'HH:mm',
+    titleFormat: 'MMMM YYYY',
+    //minTime: gon.mintime,
+    //maxTime: gon.maxtime,
+    minTime: gon.calendar_hours[0],
+    maxTime: gon.calendar_hours[1],
+    scrollTime: "00:00:00",
+    views: {
+      week: {
+        columnFormat: 'ddd D'
+      }
+    },
+    buttonText: { 
+      prev: '<',
+      next: '>',
+      today: 'hoje',
+      month: 'mes',
+      week: 'semana',
+      day: 'dia'
+    },
+    events: '/fullcalendar_events.json',
+      eventColor: '#1484E6'
+  
+  });
+
   var subjectNames = new Array();
   var subjectGrades = new Array();
   var averageGrades = new Array();
@@ -75,46 +115,5 @@ function js(){
   }
 
   new Chart(ctx).Line(data, options);
-
-  $('#calendar').fullCalendar({
-    defaultView: "agendaWeek", 
-    theme: true,
-    height: "auto",
-    themeButtonIcons: false,
-    header: {
-      left: 'today prev,next',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    monthNames: ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    allDaySlot: false,
-    slotDuration: "01:00:00",
-    axisFormat: 'HH:mm',
-    timeFormat: 'HH:mm',
-    titleFormat: 'MMMM YYYY',
-    //minTime: gon.mintime,
-    //maxTime: gon.maxtime,
-    minTime: gon.calendar_hours[0],
-    maxTime: gon.calendar_hours[1],
-    scrollTime: "00:00:00",
-    views: {
-      week: {
-        columnFormat: 'ddd D'
-      }
-    },
-    buttonText: { 
-      prev: '<',
-      next: '>',
-      today: 'hoje',
-      month: 'mes',
-      week: 'semana',
-      day: 'dia'
-    },
-    events: '/fullcalendar_events.json',
-      eventColor: '#1484E6'
-  
-  });
 };
 jQuery(document).on("ready page:change", function() { js() })
