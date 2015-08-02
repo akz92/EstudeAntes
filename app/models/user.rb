efbd257b49ac5f2d9339b9175554ba81fdf9ece6
akz92 <aridemoulin@gmail.com>
2015-08-02 16:01:07 -0300

    simplified omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,19 +16,17 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     if self.where(email: auth.info.email).exists?
       return_user = self.where(email: auth.info.email).first
-      return_user.provider = auth.provider
-      return_user.uid = auth.uid
-      return_user.name = auth.info.name
     else
       return_user = self.create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.name = auth.info.name
         user.password = Devise.friendly_token[0,20]
         user.email = auth.info.email
         user.skip_confirmation!
       end
     end
+    return_user.provider = auth.provider
+    return_user.uid = auth.uid
+    return_user.name = auth.info.name
+
     return_user
   end
 end
