cdba6920cabd3556c21bb77dfcce6754bc9a9498
akz92 <aridemoulin@gmail.com>
2015-08-03 14:22:22 -0400

    add space after #

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def show
    # filter the params[:id] here to allow only certain values like
-   #if params[:id].match /browse|help/
+   # if params[:id].match /browse|help/
    #  render :partial => params[:id]
-   #else
+   # else
    #  render :file => "/path/to/some/404_template", :status => 404
-   #end
+   # end
   end
 
   def help
