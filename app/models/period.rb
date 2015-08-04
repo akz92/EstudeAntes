836dd656753b26efd0661c645abfce3fb5aab709
akz92 <aridemoulin@gmail.com>
2015-08-04 14:02:48 -0300

    documented Period model methods

--- app/models/period.rb
+++ app/models/period.rb
@@ -10,6 +10,9 @@ class Period < ActiveRecord::Base
   validates_uniqueness_of :number
   validates_uniqueness_of :is_current, if: :true?
 
+  # Fetches the minimal start time and the maximum end time from all events from the Period
+  #
+  # @return [Array] a 2 element array containing the first and last hour to use as minTime and maxTime in FullCalendar
   def calendar_hours
     if self.events.empty?
       hours = ['06:00', '22:00']
@@ -20,6 +23,9 @@ class Period < ActiveRecord::Base
     hours
   end
 
+  # Converts an array of events JSONs into one single events JSON
+  #
+  # @return [Json] one single events JSON
   def get_events
     events = []
 
@@ -31,6 +37,9 @@ class Period < ActiveRecord::Base
     events
   end
 
+  # Defines the Period as current based on its start and end dates
+  #
+  # @return [Period] itself as current if it's current
   def is_current?
     if Date.today.between?(self.start_date, self.end_date)
       self.is_current = true
@@ -43,6 +52,9 @@ class Period < ActiveRecord::Base
     self.is_current ||= false
   end
 
+  # Checks if a given Period is current
+  #
+  # @return [Boolean] true if the Period is current
   def true?
     self.is_current == true
   end
