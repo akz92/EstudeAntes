dcd8ddd9e009b2aec781b25bf580d03a5c20bb2a
akz92 <aridemoulin@gmail.com>
2015-08-04 19:15:22 -0300

    started using chart.js gem and separated fullcalendar from chart.js in period JS

--- app/assets/javascripts/periods.js
+++ app/assets/javascripts/periods.js
@@ -1,46 +1,3 @@
-function fullCalendar(){
-  $('#calendar').fullCalendar({
-    defaultView: "agendaWeek", 
-    theme: true,
-    height: "auto",
-    themeButtonIcons: false,
-    header: {
-      left: 'today prev,next',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    monthNames: ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    allDaySlot: false,
-    slotDuration: "01:00:00",
-    axisFormat: 'HH:mm',
-    timeFormat: 'HH:mm',
-    titleFormat: 'MMMM YYYY',
-    //minTime: gon.mintime,
-    //maxTime: gon.maxtime,
-    minTime: gon.calendar_hours[0],
-    maxTime: gon.calendar_hours[1],
-    scrollTime: "00:00:00",
-    views: {
-      week: {
-        columnFormat: 'ddd D'
-      }
-    },
-    buttonText: { 
-      prev: '<',
-      next: '>',
-      today: 'hoje',
-      month: 'mes',
-      week: 'semana',
-      day: 'dia'
-    },
-    events: '/fullcalendar_events.json',
-      eventColor: '#1484E6'
-  
-  });
-}
-
 function lineChart(){
   var subjectNames = [];
   var subjectGrades = [];
@@ -66,13 +23,6 @@ function lineChart(){
         pointStrokeColor : "#fff",
         data : subjectGrades
       },
-      //{
-      //  fillColor : "rgba(100,100,180,0.5)",
-      //  strokeColor : "rgba(100,100,180,1)",
-      //  pointColor : "rgba(100,100,180,1)",
-      //  pointStrokeColor : "#fff",
-      //  data : subjectGrades
-      //},
       {
         fillColor : "rgba(220,50,50,0.3)",
         strokeColor : "#E69700",
@@ -80,30 +30,10 @@ function lineChart(){
         pointStrokeColor : "rgba(0,0,0,0)",
         data : averageGrades
       }
-      //{
-      //  fillColor : "rgba(220,50,50,0.3)",
-      //  strokeColor : "rgba(220,50,50,1)",
-      //  pointColor : "rgba(0,0,0,0)",
-      //  pointStrokeColor : "rgba(0,0,0,0)",
-      //  data : averageGrades
-      //}
-      //{
-      //  fillColor : "rgba(220,50,50,0.3)",
-      //  strokeColor : "rgba(220,50,50,1)",
-      //  pointColor : "rgba(220,50,50,1)",
-      //  pointStrokeColor : "#fff",
-      //  pointDot: false,
-      //  data : averageGrades
-      //}
     ]
   };
 
   var ctx = document.getElementById("canvas").getContext("2d");
-  var pixelRatio = window.devicePixelRatio || 1;
-  var width = $('canvas').parent().width();
-  var height = $('canvas').parent().height();
-  ctx.canvas.width = width / pixelRatio;
-  ctx.canvas.height = (1.5 * height) / pixelRatio;
   var myNewChart = new Chart(ctx).Line(data);
 
   var options = {
@@ -112,10 +42,10 @@ function lineChart(){
     scaleStepWidth  : 10,
     scaleStartValue : 0,
     datasetFill : false,
-    //responsive: true,
+    responsive: true,
     //maintainAspectRatio: true
   };
 
   new Chart(ctx).Line(data, options);
 }
-jQuery(document).on("ready page:change", function() { fullCalendar(); lineChart();});
+jQuery(document).on("ready page:change", function() { lineChart();});
