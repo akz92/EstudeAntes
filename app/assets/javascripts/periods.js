1fc758ca3fd45f94a414ac51c3d9f075fb54c68b
akz92 <aridemoulin@gmail.com>
2015-08-05 06:43:49 -0300

    changed the main chart to Bar

--- app/assets/javascripts/periods.js
+++ app/assets/javascripts/periods.js
@@ -1,23 +1,61 @@
-function lineChart(){
-  var data = []
+//function lineChart(){
+//  var data = []
+//
+//  for(var i = 0; i < gon.subjects.length; i++){
+//    data.push({
+//                value: gon.subjects[i].grade,
+//                color:"#1484E6",
+//                //highlight: "#FF5A5E",
+//                label: gon.subjects[i].name
+//              });
+//  }
+//
+//  var ctx = document.getElementById("canvas").getContext("2d");
+//  var myNewChart = new Chart(ctx).PolarArea(data);
+//
+//  var options = {
+//    responsive: true,
+//    //maintainAspectRatio: true
+//  };
+//
+//  new Chart(ctx).PolarArea(data, options);
+//}
+
+function barChart(){
+  var subjectNames = [];
+  var subjectGrades = [];
+  var averageGrades = [];
 
   for(var i = 0; i < gon.subjects.length; i++){
-    data.push({
-                value: gon.subjects[i].grade,
-                color:"#F7464A",
-                highlight: "#FF5A5E",
-                label: gon.subjects[i].name
-              });
+    subjectNames.push(gon.subjects[i].name);
+    if (gon.subjects[i].value === 0) {
+      subjectGrades.push(gon.subjects[i].grade);
+    } else {
+      subjectGrades.push((100 * gon.subjects[i].grade)/gon.subjects[i].value);
+    }
+    averageGrades.push(60);
+  }
+
+  var data = {
+    labels : subjectNames,
+    datasets : [
+      {
+        fillColor:"#1484E6",
+        data : subjectGrades
+      }
+    ]
   }
 
   var ctx = document.getElementById("canvas").getContext("2d");
-  var myNewChart = new Chart(ctx).PolarArea(data);
+  var myBarChart = new Chart(ctx).Bar(data);
 
   var options = {
-    responsive: true,
+    barValueSpacing : 30,
+    responsive : true,
     //maintainAspectRatio: true
   };
 
-  new Chart(ctx).PolarArea(data, options);
+  new Chart(ctx).Bar(data, options);
+
 }
-jQuery(document).on("ready page:change", function() { lineChart();});
+jQuery(document).on("ready page:change", function() { barChart();});
