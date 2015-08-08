0dda9f8bea1c700ffe8b1b3b98c7003bbab332d0
akz92 <aridemoulin@gmail.com>
2015-08-08 12:00:15 -0300

    removed unnecessary subjects helper

--- app/helpers/subjects_helper.rb
+++
@@ -1,9 +0,0 @@
-module SubjectsHelper
-  # Creates a hidden div containing the Test's/Project's note
-  def dynamic_div(id, note)
-    div = "##{id}.accordian-body.collapse\n  %b> Matéria:\n  #{note}"
-    engine = Haml::Engine.new(div)
-    div = engine.render
-    return div
-  end
-end
