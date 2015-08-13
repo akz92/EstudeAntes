9fa001116cbe67c0b396664c5ba2a3698f60e484
akz92 <aridemoulin@gmail.com>
2015-08-13 08:55:53 -0400

    removed old flash messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 module ApplicationHelper
-  # Set the style of the Bootstrap Message
-  #
-  # @param level [String] the alert type
-  # @return [String] the corresponding alert type
-  def flash_class(level)
-    case level
-    when 'success' then 'alert alert-success'
-    when 'error' then 'alert alert-danger'
-    when 'alert' then 'alert alert-warning'
-    else 'alert alert-info'
-    end
-  end
-
   def resource_name
     :user
   end
