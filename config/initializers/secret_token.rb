b64b71b003a51ce14f0250beacc14a3019c90c13
akz92 <aridemoulin@gmail.com>
2015-08-14 09:58:47 -0400

    trying to use secrets.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Estudeantes::Application.config.secret_key_base = '99c1b2ca70d5172640dcde21c930dac0126d2c1fe468d15718dc69834ddf1648dec159fa4e6afbfb09b4310051588b35d57cd91ef3b06f35b96ea385c8afe8e4'
-Estudeantes::Application.config.secret_token = ENV['SECRET_TOKEN']
+# Estudeantes::Application.config.secret_key_base = '99c1b2ca70d5172640dcde21c930dac0126d2c1fe468d15718dc69834ddf1648dec159fa4e6afbfb09b4310051588b35d57cd91ef3b06f35b96ea385c8afe8e4'
+# Estudeantes::Application.config.secret_token = ENV['SECRET_TOKEN']
