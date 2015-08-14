25c591cf7a8299117133cc5ba5ad42aa481b96d8
akz92 <aridemoulin@gmail.com>
2015-08-14 09:16:09 -0400

    omniauth controller fully tested

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -9,23 +9,21 @@ describe User::OmniauthCallbacksController do
 
   describe "#facebook" do
 
-    it "should successfully create a user" do
+    it 'successfully creates a user' do
       expect {
         post :facebook, provider: :facebook
       }.to change{ User.count }.by(1)
     end
 
-    it "should successfully create a session" do
-      expect(session[:user_id]).to be_nil
-      post :facebook, provider: :facebook
-      expect(session[:user_id]).not_to be_nil
-    end
-
-    it "should redirect the user to the root url" do
+    it 'redirects the user to the root url' do
       post :facebook, provider: :facebook
       expect(response).to redirect_to root_url
     end
 
+    it 'signs the user in' do
+      post :facebook, provider: :facebook
+      expect(warden.authenticated?(:user)).to be true
+    end
   end
 
 end
