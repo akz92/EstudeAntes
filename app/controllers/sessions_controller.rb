8a283fca87c26b3fc13033cc8c1d36d32c9b308b
akz92 <aridemoulin@gmail.com>
2015-08-16 15:50:59 -0300

    removed sessions controller

--- app/controllers/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class SessionsController < ApplicationController
-  # Creates a user from Facebook or updates an existing one with his Facebook credentials
-  def create
-    user = User.from_omniauth(env['omniauth.auth'])
-    session[:user_id] = user.id
-    redirect_to root_url
-  end
-end
